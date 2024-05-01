6641cca761d515f35a737dff8597a1378956611f
pavelbekarev <bekarev.ps@gmail.com>
2024-05-01 22:51:39 +0500

    добавил услуги, их описание

--- src/pages/Services.tsx
+++ src/pages/Services.tsx
@@ -7,7 +7,9 @@ export const Services = (): JSX.Element => {
     const [item1Visible, setitem1Visible]= useState<Boolean>(false);
     const [item2Visible, setitem2Visible]= useState<Boolean>(false);
     const [item3Visible, setitem3Visible]= useState<Boolean>(false);
-
+    const [item4Visible, setitem4Visible]= useState<Boolean>(false);
+    const [item5Visible, setitem5Visible]= useState<Boolean>(false);
+    const [item6Visible, setitem6Visible]= useState<Boolean>(false);
 
     return (
         <>
@@ -32,7 +34,7 @@ export const Services = (): JSX.Element => {
                                                 setitem1Visible(!item1Visible);
                                             }}
                                         >
-                                            Консультация
+                                            Создание стратегии развития и цели на полгода или год
                                         </div>
                                         {
                                             item1Visible 
@@ -41,17 +43,19 @@ export const Services = (): JSX.Element => {
                                                 className={`accordion-content ${item1Visible ? "accordion__animation" : ""}`}
                                                 // style={(item1Visible || !item1Visible) && {transition: "0.3s all ease"}}
                                             >
+                                                <span>
+                                                    <i>
+                                                        После проведения сессии (минимум 4 часа сессия или по 2 часа в несколько дней) 
+                                                    </i>
+                                                </span>
+                                                <h4>Результаты</h4>
                                                 <ul className="list">
                                                     <li>—   Сбор анамнеза. Выявление психологических и физиологических причин лишнего веса</li>
                                                     <li>—   Анализ рациона по дневнику питания</li>
                                                     <li>—   Рекомендации по питанию, изменению пищевого поведения, регуляции эмоционального состояния в зависимости от вашего запроса на консультацию</li>
                                                 </ul>
                                                 <div className="prices__info">
-                                                    <span>ONLINE</span>
-                                                    <div className="price">
-                                                        <span>50 минут</span>
-                                                        <span>5000 руб. (80$)</span>
-                                                    </div>
+                                                   
                                                 </div>
                                                 <a href="" className="registration__button">
                                                     Записаться
@@ -66,7 +70,7 @@ export const Services = (): JSX.Element => {
                                                 setitem2Visible(!item2Visible);
                                             }}
                                         >
-                                            Индивидуальное сопровождение
+                                            Сессия по созданию ценностей и этики компании
                                         </div>
                                         {
                                             item2Visible 
@@ -74,7 +78,22 @@ export const Services = (): JSX.Element => {
                                                 <div 
                                                     className="accordion-content"
                                                 >
-                                                    Content for section 2
+                                                    <span>
+                                                        <i>
+                                                            Проведение 2-х часовой сессии со всеми топами компании.
+                                                        </i>
+                                                    </span>
+                                                    <h4>Результаты</h4>
+                                                    <ul className="list">
+                                                        <li>—   Общее виденье и правила как осуществляется работа внутри компании  (взаимодействие и подход к работе).</li>
+                                                        <li>—   Оформленный в документ для ознакомления новым сотрудникам при трудоустройстве.</li>
+                                                    </ul>
+                                                    <div className="prices__info">
+                                                        
+                                                    </div>
+                                                    <a href="" className="registration__button">
+                                                        Записаться
+                                                    </a>
                                                 </div>
                                         }
                                     </div>
@@ -85,7 +104,7 @@ export const Services = (): JSX.Element => {
                                                 setitem3Visible(!item3Visible);
                                             }}
                                         >
-                                            Консультация
+                                            Оргструктура компании
                                         </div>
                                         {
                                             item3Visible 
@@ -93,7 +112,123 @@ export const Services = (): JSX.Element => {
                                                 <div 
                                                     className="accordion-content"
                                                 >
-                                                    Content for section 3
+                                                    <span>
+                                                        Иерархия и порядок взаимодействия отделов
+                                                    </span>
+                                                    <h4>Результаты</h4>
+                                                    <ul className="list">
+                                                        <li>—   Структура компании</li>
+                                                        <li>—   Четкие показатели каждого отдела</li>
+                                                        <li>—   Стратегия роста компании (куда какие сотрудники нужны)</li>
+                                                    </ul>
+                                                    <div className="prices__info">
+                                                        
+                                                    </div>
+                                                    <a href="" className="registration__button">
+                                                        Записаться
+                                                    </a>
+                                                </div>
+                                        }
+                                    </div>
+                                    <div className="accordion-item">
+                                        <div 
+                                            className="accordion-header"
+                                            onClick={() => {
+                                                setitem4Visible(!item4Visible);
+                                            }}
+                                        >
+                                            Оформление бизнес процессов и распределение функционала между подразделениями
+                                        </div>
+                                        {
+                                            item4Visible 
+                                            &&
+                                                <div 
+                                                    className="accordion-content"
+                                                >
+                                                    <span>
+                                                        Задействованы руководители отделов. Срок 1 месяц (зависит от объема компании)
+                                                    </span>
+                                                    <h4>Результаты</h4>
+                                                    <ul className="list">
+                                                        <li>—   Прописанный регламент процессов каждой должности  и показатели</li>
+                                                        <li>—   Прописанный функционал должности и показатели.</li>
+                                                    </ul>
+                                                    <div className="prices__info">
+                                                        
+                                                    </div>
+                                                    <a href="" className="registration__button">
+                                                        Записаться
+                                                    </a>
+                                                </div>
+                                        }
+                                    </div>
+
+                                    <div className="accordion-item">
+                                        <div 
+                                            className="accordion-header"
+                                            onClick={() => {
+                                                setitem5Visible(!item5Visible);
+                                            }}
+                                        >
+                                            План мотивационных мероприятий для персонала
+                                        </div>
+                                        {
+                                            item5Visible 
+                                            &&
+                                                <div 
+                                                    className="accordion-content"
+                                                >
+                                                    <span>
+                                                    В процессе работы провожу опрос, анкетирование или интервью, формирую план исходя показателям (на командообразование, улучшение коммуникаций, обучающие, анализирующие, планирование …) 
+                                                    </span>
+                                                    <h4>Результаты</h4>
+                                                    <ul className="list">
+                                                        <li>—   Повышенная лояльность и вовлеченность сотрудников</li>
+                                                        <li>—   План и сроки мероприятий</li>
+                                                        <li>—   Профилактика выгорания</li>
+                                                        <li>—   Примерный бюджет на год</li>
+                                                    </ul>
+                                                    <div className="prices__info">
+                                                        
+                                                    </div>
+                                                    <a href="" className="registration__button">
+                                                        Записаться
+                                                    </a>
+                                                </div>
+                                        }
+                                    </div>
+
+                                    <div className="accordion-item">
+                                        <div 
+                                            className="accordion-header"
+                                            onClick={() => {
+                                                setitem6Visible(!item6Visible);
+                                            }}
+                                        >
+                                            Книга продукта
+                                        </div>
+                                        {
+                                            item6Visible 
+                                            &&
+                                                <div 
+                                                    className="accordion-content"
+                                                >
+                                                    <span>
+                                                        Работа с экспертом и тренером для отдела продаж.
+                                                    </span>
+                                                    {/* <h4>Результаты</h4>
+                                                    <ul className="list">
+                                                        <li>—   Повышенная лояльность и вовлеченность сотрудников</li>
+                                                        <li>—   План и сроки мероприятий</li>
+                                                        <li>—   Профилактика выгорания</li>
+                                                        <li>—   Примерный бюджет на год</li>
+                                                    </ul> */}
+                                                    <div className="prices__info">
+                                                        
+                                                    </div>
+                                                    <a href="" className="registration__button">
+                                                        Записаться
+                                                    </a>
                                                 </div>
                                         }
                                     </div>
